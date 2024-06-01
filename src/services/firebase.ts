b6806c1ea2c927745f728261db099dddbd84da6f
Fábio Valentim <maitabom@gmail.com>
2024-06-01 17:45:32 -0300

    Atualização de configurações e ocultação de dados sensíveis

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -3,19 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+
+import firebaseConfigData from "./keys/firebase";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyB_RkR5EigqPCIukhtkERsHTpUcEFuOWjY",
-  authDomain: "webcarros-fdd75.firebaseapp.com",
-  projectId: "webcarros-fdd75",
-  storageBucket: "webcarros-fdd75.appspot.com",
-  messagingSenderId: "898035068755",
-  appId: "1:898035068755:web:3e538c3c1c805bef7dfba6",
-};
-
+const firebaseConfig = firebaseConfigData;
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
